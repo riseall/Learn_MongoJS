87ad1102a66634d0f1c27cf12b5b83e9dbaeffcb
Rizal <rizalnug2@gmail.com>
2025-01-01 11:17:06 +0700

    find data

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/movie_db').then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/movie_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const movieSchema = new mongoose.Schema({
-    title: String,
-    year: Number,
-    rating: Number,
-    genre: String,
-    director: String,
+  title: String,
+  year: Number,
+  rating: Number,
+  genre: String,
+  director: String,
 });
 
-const Movie = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model("Movie", movieSchema);
 
-const movie = new Movie({
-    title: 'The Shawshank Redemption',
-    year: 1994,
-    rating: 9.3,
-    genre: 'Drama',
-    director: 'Frank Darabont',
-});
+// const movie = new Movie({
+//     title: 'The Shawshank Redemption',
+//     year: 1994,
+//     rating: 9.3,
+//     genre: 'Drama',
+//     director: 'Frank Darabont',
+// });
 
-movie.save();
+// Melihat data
+Movie.findById("67741c0b4ea062817761dda7")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
 
-console.log(movie);
+// Movie.insertMany([
+//     {
+//         title: 'The Shawshank Redemption',
+//         year: 1994,
+//         rating: 9.3,
+//         genre: 'Drama',
+//         director: 'Frank Darabont',
+//     },
+//     {
+//         title: 'The Godfather',
+//         year: 1972,
+//         rating: 9.2,
+//         genre: 'Crime',
+//         director: 'Francis Ford Coppola',
+//     },
+//     {
+//         title: 'Pulp Fiction',
+//         year: 1994,
+//         rating: 8.9,
+//         genre: 'Crime',
+//         director: 'Quentin Tarantino',
+//     },
+//     {
+//         title: 'The Dark Knight',
+//         year: 2008,
+//         rating: 9.0,
+//         genre: 'Action',
+//         director: 'Christopher Nolan',
+//     },]).then((result) => {
+//         console.log('It worked!');
+//         console.log(result);
+//     }).catch((error) => {
+//         console.error('Error inserting data:', error);
+//     });
