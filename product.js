8c9b18f388c4b2d803b0425529e62c5e2f060d23
Rizal <rizalnug2@gmail.com>
2025-01-01 22:05:41 +0700

    membuat schema product mongoose

---
+++ product.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/product_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  size: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+    maxLength: 150,
+  },
+  condition: {
+    type: String,
+    enum: ["baru", "bekas"],
+    default: "baru",
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  availability: {
+    online: {
+      type: Boolean,
+      required: true,
+    },
+    offline: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
